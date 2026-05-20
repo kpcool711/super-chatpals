948b67f980476c12a0a26413b81e17272795704c
kp <kpcool.711@gmail.com>
2026-05-20 17:51:01 +0200

    Add files via upload

---
+++ OneSignalSDKWorker.js
@@ -0,0 +1,3 @@
+// OneSignal Web Push Service Worker
+// This handles incoming push notifications from the OneSignal service.
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
